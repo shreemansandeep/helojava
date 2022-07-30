7590a6faa3f685714ca1b0b65b40c4e1ba017636
shreemansandeep <81627168+shreemansandeep@users.noreply.github.com>
2022-07-30 13:32:31 +0530

    Update helojava.java

--- helojava.java
+++ helojava.java
@@ -1,6 +1,14 @@
+package crunch;
 
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, java2!"); 
-    }
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloWorld extends HttpServlet {
+  public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+      throws ServletException, IOException {
+    PrintWriter out = response.getWriter();
+    out.println("Hello World");
+  }
 }
